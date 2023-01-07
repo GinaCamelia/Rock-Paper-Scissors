392f65b92df796e637334cd2994ab8252734dbbb
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-01-07 14:12:49 +0000

    declared variables for user and computer's choice, and to keep track of score

---
+++ script.js
@@ -0,0 +1,14 @@
+    // This function allows the user to play a game of rock, paper, scissors
+function playGame() {
+    // Declare variables for the user's and computer's choice
+    let userChoice;
+    let computerChoice;
+
+    //Declare variables to keep track of the score
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+
+    //
+
+}
