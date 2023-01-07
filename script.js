1b18934f1e46bcff1a83b2263fdad0d50dbd1728
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-01-07 14:21:59 +0000

    created a while loop to allow user to play multiple rounds in a single game and then I then assigned  computer's choices at random to ensure fair game

--- script.js
+++ script.js
@@ -4,11 +4,24 @@ function playGame() {
     let userChoice;
     let computerChoice;
 
-    //Declare variables to keep track of the score
+    // Declaring variables to keep track of the score
     let wins = 0;
     let losses = 0;
     let ties = 0;
 
-    //
+    // Using a while loop to allow user to play multiple games
+    while (true) {
+        // Using a loop to allow user to playing multiple rounds in a single game
+        for (i = 0; i < 3; i++) {
+            userChoice = prompt("Please choose rock, paper, scissors:");
+            userChoice = userChoice.toLowerCase();
+
+            // Getting the computer's choice
+            let words = ["rock", "paper", "scissors"];
+            computerChoice = words[Math.floor(Math.random() * words.length)];
+
+            
+        }
+    }
 
 }
